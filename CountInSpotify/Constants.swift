a031f20c7ebb06a8381f4572e63bc82f0239e791
Killian-Kenny <>
2023-09-20 15:42:38 +1200

    Add Spotify framework

---
+++ CountInSpotify/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  CountInSpotify
+//
+//  Created by Killian-Kenny on 20/09/2023.
+//
+
+import Foundation
+
+struct Constants {
+    static let spotifyClientID = "841fa0a3ce334667aca0223c99dbe3f2"
+    static let spotifyRedirectURL = URL(string: "http://appenvision.co/count-in-spotify")!
+}
--- CountInSpotify/SceneDelegate.swift
+++ CountInSpotify/SceneDelegate.swift
@@ -7,9 +7,21 @@
 
 import UIKit
 
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+import SpotifyiOS
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate, SPTAppRemoteDelegate, SPTAppRemotePlayerStateDelegate {
 
     var window: UIWindow?
+    
+    lazy var appRemote: SPTAppRemote = {
+        let configuration = SPTConfiguration(clientID: Constants.spotifyClientID,
+                                             redirectURL: Constants.spotifyRedirectURL)
+        let appRemote = SPTAppRemote(configuration: configuration, logLevel: .debug)
+        appRemote.connectionParameters.accessToken = self.spotifyAccessToken
+        appRemote.delegate = self
+        return appRemote
+    }()
+    var spotifyAccessToken: String?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
@@ -45,6 +57,40 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
+    
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        guard let url = URLContexts.first?.url else {
+            return
+        }
 
+        let parameters = appRemote.authorizationParameters(from: url);
+
+        if let access_token = parameters?[SPTAppRemoteAccessTokenKey] {
+            appRemote.connectionParameters.accessToken = access_token
+            spotifyAccessToken = access_token
+        } else if let error_description = parameters?[SPTAppRemoteErrorDescriptionKey] {
+            // Show the error
+        }
+    }
+
+    //MARK: - SPTAppRemoteDelegate
+    
+    func appRemoteDidEstablishConnection(_ appRemote: SPTAppRemote) {
+      print("connected")
+    }
+    
+    func appRemote(_ appRemote: SPTAppRemote, didDisconnectWithError error: Error?) {
+      print("disconnected")
+    }
+    
+    func appRemote(_ appRemote: SPTAppRemote, didFailConnectionAttemptWithError error: Error?) {
+      print("failed")
+    }
+    
+    //MARK: - PTAppRemotePlayerStateDelegate
+    
+    func playerStateDidChange(_ playerState: SPTAppRemotePlayerState) {
+      print("player state changed")
+    }
 }
 
