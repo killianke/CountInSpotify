a193360fd562c5c419b2eb2486cdf72deddc923b
Killian-Kenny <>
2023-09-29 23:52:50 +1300

    Add track model

---
+++ CountInSpotify/Models/Track.swift
@@ -0,0 +1,14 @@
+//
+//  Track.swift
+//  CountInSpotify
+//
+//  Created by Killian-Kenny on 29/09/2023.
+//
+
+import Foundation
+
+struct Track {
+    var spotifyID: String
+    var startTime: Int
+    var bpm: Double
+}
