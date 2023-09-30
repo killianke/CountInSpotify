2fd66662b6b6e5835cfb8e1b053e46968174dec4
Killian-Kenny <>
2023-09-30 23:23:45 +1300

    Update metronome to run for specified number of bars

--- CountInSpotify/Metronome.swift
+++ CountInSpotify/Metronome.swift
@@ -10,6 +10,8 @@ import AVFoundation
 
 class Metronome {
     
+    var didStopClosure: (()->())?
+    
     private var engine = AVAudioEngine()
     private var player = AVAudioPlayerNode()
     
@@ -26,19 +28,23 @@ class Metronome {
     }
     private var timerEventCounter: Int = 1
     private var currentBeat: Int = 1
+    private var currentBar: Int = 1
     private var timer: Timer! = nil
+    private var maxBars: Int?
     
     private enum MetronomeState { case running; case stopped }
     private var state: MetronomeState = .stopped
     
-    init() {
+    init(bpm: Double) {
+        self.bpm = bpm
         setupAudio()
     }
     
     //MARK: Public interface
     
     func start(forBars numberOfBars: Int? = nil) {
-        
+        maxBars = numberOfBars
+
         state = .running
         scheduleFirstBuffer()
         createTimer()
@@ -49,8 +55,11 @@ class Metronome {
         timer.invalidate()
         timerEventCounter = 1
         currentBeat = 1
+        currentBar = 1
+        
+        didStopClosure?()
     }
-
+    
     //MARK: Private funcs
     
     private func setupAudio() {
@@ -84,7 +93,7 @@ class Metronome {
     
     private func createBuffer(withFile file: AVAudioFile) -> AVAudioPCMBuffer? {
         do {
-            var periodLengthInSamples = periodLength * sampleRate
+            let periodLengthInSamples = periodLength * sampleRate
             let buffer = AVAudioPCMBuffer(
                 pcmFormat: file.processingFormat,
                 frameCapacity: AVAudioFrameCount(periodLengthInSamples))!
@@ -96,37 +105,41 @@ class Metronome {
         }
         return nil
     }
-        
+    
     private func createTimer() {
         
         // Compute interval for 2 events per period and set up timer
         let timerIntervallInSamples = 0.5 * self.periodLength
         
         timer = Timer.scheduledTimer(withTimeInterval: timerIntervallInSamples, repeats: true) { timer in
-
+            
             // Schedule next buffer at 1st, 3rd, 5th & 7th timerEvent
             switch self.timerEventCounter {
             case 7:
                 // Schedule beat 1 sound
-                self.player.scheduleBuffer(self.buffer1, at:nil, options: [], completionHandler: nil)
+                self.player.scheduleBuffer(self.buffer1, at: nil, options: [], completionHandler: nil)
                 
             case 1, 3, 5:
                 // Schedule subdivision sound
-                self.player.scheduleBuffer(self.buffer2, at:nil, options: [], completionHandler: nil)
+                self.player.scheduleBuffer(self.buffer2, at: nil, options: [], completionHandler: nil)
             default:
                 break
             }
-            
             // Display current beat & increase currentBeat (1...4) at 2nd, 4th, 6th & 8th timerEvent
             if self.timerEventCounter % 2 == 0 {
                 self.currentBeat += 1
                 if self.currentBeat > 4 {
+                    self.currentBar += 1
                     self.currentBeat = 1
                 }
             }
             
             // Increase timerEventCounter, two events per beat.
             self.timerEventCounter += 1; if self.timerEventCounter > 8 {self.timerEventCounter = 1}
+            
+            if self.currentBar == self.maxBars && self.timerEventCounter == 8 {
+                self.stop()
+            }
         }
     }
     
