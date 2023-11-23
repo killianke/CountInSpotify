a11d03d664d11222592be5e59d59ac7c9d4bdde5
Killian-Kenny <>
2023-11-23 18:54:06 +1300

    Fixes crash when dismissing AppStore vc

--- CountInSpotify/SharedViews/StoreView.swift
+++ CountInSpotify/SharedViews/StoreView.swift
@@ -11,22 +11,17 @@ import SpotifyiOS
 
 struct StoreView: View, UIViewControllerRepresentable {
 
-    typealias UIViewControllerType = SKStoreProductViewController
+    typealias UIViewControllerType = StoreViewController
 
     @Binding var showStoreView: Bool
     
     let appId: String
     
-    func makeUIViewController(context: Context) -> SKStoreProductViewController {
-        let viewController = SKStoreProductViewController()
-        viewController.loadProduct(withParameters: [SKStoreProductParameterITunesItemIdentifier: appId])
-        viewController.delegate = context.coordinator
-        return viewController
+    func makeUIViewController(context: UIViewControllerRepresentableContext<StoreView>) -> StoreViewController {
+        return StoreViewController(coordinator: context.coordinator, appId: appId)
     }
-    
-    func updateUIViewController(_ uiViewController: SKStoreProductViewController, context: Context) {
-        
-    }
+
+    func updateUIViewController(_ uiViewController: StoreViewController, context: UIViewControllerRepresentableContext<StoreView>) {}
     
     func makeCoordinator() -> Coordinator {
         Coordinator(parent: self)
@@ -41,6 +36,40 @@ struct StoreView: View, UIViewControllerRepresentable {
         
         func productViewControllerDidFinish(_ viewController: SKStoreProductViewController) {
             parent.showStoreView = false
+        }
+    }
+}
+
+class StoreViewController: UIViewController {
+    private let coordinator: StoreView.Coordinator
+    private let appId: String
+    
+    private var storeController: SKStoreProductViewController?
+    
+    init(coordinator: StoreView.Coordinator, appId: String) {
+        self.coordinator = coordinator
+        self.appId = appId
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+                
+        storeController = SKStoreProductViewController()
+        storeController?.delegate = coordinator
+        
+        storeController?.loadProduct(withParameters: [SKStoreProductParameterITunesItemIdentifier: appId])
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if let vc = storeController {
+            present(vc, animated: true)
         }
     }
 }
